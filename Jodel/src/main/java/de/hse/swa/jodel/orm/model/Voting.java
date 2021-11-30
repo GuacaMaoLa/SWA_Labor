e15f02fe54daab71b94cffb9fe7387c89cf93df3
Claudius Deuschle <claudiusdeuschle@googlemail.com>
2021-11-30 21:51:55 +0100

    Added models for comment,post and voting

--- Jodel/src/main/java/de/hse/swa/jodel/orm/model/Voting.java
+++ Jodel/src/main/java/de/hse/swa/jodel/orm/model/Voting.java
@@ -30,9 +30,54 @@ public class Voting implements Serializable {
     @SequenceGenerator(name = "voteSeq", sequenceName = "ZSEQ_VOTE_ID", allocationSize = 1, initialValue = 10)
     @GeneratedValue(generator = "voteSeq")
 	private Long id;
-	
-	@Column(name = "VALUE")
+
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "commentId")
+	private Long commentId;
+
+
+	@Column(name = "authorId")
+	private Long authorId;
+
+	@Column(name = "value")
 	private int value;
-	
+
+	public Voting() {
+
+	}
+
+	public Voting(int value) {
+		this.value = value;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Long getAuthorId() {
+		return authorId;
+	}
+
+	public void setAuthorId(Long authorId) {
+		this.authorId = authorId;
+	}
+
+	public Long getCommentId() {
+		return commentId;
+	}
+
+	public void setCommentId(Long commentId) {
+		this.commentId = commentId;
+	}
 
 }
