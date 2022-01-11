0977d0a534c54853069702bba6b8fe14366d2f14
Claudius Deuschle <74427926+clde1407@users.noreply.github.com>
2022-01-11 21:48:08 +0100

    Delete Jodel directory

--- Jodel/bin/main/import.sql
+++
@@ -1,5 +0,0 @@
--- Some initial data
-INSERT INTO TUSER (id, username, password) 
-       VALUES ("1", "Ausername", "Apassword");
-INSERT INTO TUSER (id, username, password)
-       VALUES ("2", "Busername", "Bpassword");
